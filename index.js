d74a054cbb72b7ffb25e6f38c050af4a688f517d
William Warner Burke <williamburke121@gmail.com>
2022-02-03 16:24:49 -0500

    commit tree 12938370a4aea02f32b33d18af4e9d7f0cca6e83

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(drivers => drivers.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array,name){
+  return array.filter(array => array[0][0] === name[0][0])
+ }
+
+
+   
+function matchName(drivers, string){
+    return drivers.filter(drivers => drivers.name === string )
+}
